6f8f222a985c15c3b533c948720e5bee401e29f5
ArianeVargas <ariane2fernanda@gmail.com>
2023-10-05 14:57:49 -0500

    estilos de tipo

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -6,6 +6,35 @@
         emit('set-pokemon', pokemon)
     };
 
+    /* color para los tipos */
+    function getColorClass(tipo) {
+  switch (tipo) {
+    case "fire":
+      return "bg-red-500 text-white";
+    case "grass":
+      return "bg-green-500 text-white";
+    case "water":
+      return "bg-blue-500 text-white";
+    case "electric":
+      return "bg-yellow-500 text-black"; // Tipo eléctrico
+    case "ice":
+      return "bg-blue-200 text-black"; // Tipo hielo
+    case "fighting":
+      return "bg-red-700 text-white"; // Tipo lucha
+    case "poison":
+      return "bg-purple-500 text-white"; // Tipo veneno
+    case "bug":
+      return "bg-orange-900 text-white"; // Tipo insecto
+    case "flying":
+      return "bg-teal-300 text-white"; // Tipo volador
+    case "ground":
+      return "bg-green-900 text-white"; // Tipo suelo
+    // Agrega más casos según los tipos que existan en la API
+    default:
+      return "bg-gray-500 text-white"; // Clase predeterminada en caso de que el tipo no tenga una clase específica
+  }
+}
+
 </script>
 
 <template>
@@ -18,7 +47,7 @@
         </div>
 
         <ul class="grid grid-cols-6 gap-4 text-lg mt-10">
-            <li v-for="item in props.resultados" @click="pokemonSeleccionado(item)" class="border rounded-xl p-10 shadow">
+            <li v-for="item in props.resultados" :key="item.numero" @click="pokemonSeleccionado(item)" class="border rounded-xl p-10 shadow cursor-pointer">
                 <div>
                     <img :src="item.img">
                 </div>
@@ -28,8 +57,8 @@
                 <h1 class="text-center font-semibold">
                     {{ item.nombre }}
                 </h1>
-                <ul class="flex space-x-2 mt-2">
-                    <li v-for="(tipo, index) in item.tipo_pk" :key="index" class="rounded bg-blue-500 text-white px-2">
+                <ul class="flex justify-center space-x-2 mt-2">
+                    <li v-for="(tipo, index) in item.tipo_pk" :key="index" :class="getColorClass(tipo)" class="rounded bg-blue-500 text-white px-2">
                         {{ tipo }}
                     </li>
                 </ul>
